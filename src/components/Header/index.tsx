3ef05a07e28bc5f184fa5e8c8529cb7df1921945
Ricieri Rostirolla <114628700+rrostirolla@users.noreply.github.com>
2023-03-08 16:45:50 -0300

    initial commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,17 @@
-import * as S from './styles'
+import { useSelector } from 'react-redux'
 
-import { Produto } from '../../App'
+import * as S from './styles'
 
 import cesta from '../../assets/cesta.png'
 import { paraReal } from '../Produto'
 
-type Props = {
-  itensNoCarrinho: Produto[]
-  favoritos: Produto[]
-}
+import { RootReducer } from '../../store'
 
-const Header = ({ itensNoCarrinho, favoritos }: Props) => {
-  const valorTotal = itensNoCarrinho.reduce((acc, item) => {
+const Header = () => {
+  const produtos = useSelector((state: RootReducer) => state.carrinho.item)
+  const favoritos = useSelector((state: RootReducer) => state.favoritos.item)
+
+  const valorTotal = produtos.reduce((acc, item) => {
     acc += item.preco
     return acc
   }, 0)
@@ -23,7 +23,7 @@ const Header = ({ itensNoCarrinho, favoritos }: Props) => {
         <span>{favoritos.length} favoritos</span>
         <img src={cesta} />
         <span>
-          {itensNoCarrinho.length} itens, valor total: {paraReal(valorTotal)}
+          {produtos.length} itens, valor total: {paraReal(valorTotal)}
         </span>
       </div>
     </S.Header>
---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { Produto } from '../../App'
+
+type CarrinhoState = {
+  item: Produto[]
+}
+
+const initialState: CarrinhoState = {
+  item: []
+}
+
+const carrinhoSlie = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    adicionar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+
+      if (state.item.find((p) => p.id === produto.id)) {
+        alert('Item já adicionado')
+      } else {
+        state.item.push(produto)
+      }
+    }
+  }
+})
+
+export const { adicionar } = carrinhoSlie.actions
+export default carrinhoSlie.reducer
